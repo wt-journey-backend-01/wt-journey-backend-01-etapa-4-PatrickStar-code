9fcf4b38e78edc2023f1c4637b8637ce373d385b
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2025-08-31 16:29:45 -0300

    .

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,38 +1,30 @@
 const jwt = require("jsonwebtoken");
-const errorHandler = require("../utils/errorHandler");
 
-async function authMiddleware(req, res, next) {
-  try {
-    // Pega o token do header Authorization ou do cookie (opcional)
-    const token =
-      req.headers.authorization?.split(" ")[1] || req.cookies?.access_token;
-
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "Token de autenticação obrigatório." });
-    }
-
-    // Verifica o token de forma síncrona com try/catch
-    let decoded;
-    try {
-      decoded = jwt.verify(token, process.env.JWT_SECRET);
-    } catch (err) {
-      if (err.name === "TokenExpiredError") {
-        return res.status(401).json({ message: "Token expirado." });
-      } else if (err.name === "JsonWebTokenError") {
-        return res.status(401).json({ message: "Token inválido." });
-      } else {
-        return next(err);
-      }
-    }
-
-    // Adiciona os dados do token no request
-    req.user = decoded;
-    next();
-  } catch (error) {
-    next(error);
+class APIError extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+    this.name = "APIError";
   }
+}
+
+function authMiddleware(req, res, next) {
+  const cookieToken = req.cookies?.token;
+  const authHeader = req.headers["authorization"];
+  const headerToken = authHeader && authHeader.split(" ")[1];
+
+  const token = headerToken || cookieToken;
+
+  if (!token) {
+    return next(new APIError(401, "Token necessário"));
+  }
+  jwt.verify(token, process.env.JWT_SECRET || "secret", (err, user) => {
+    if (err) {
+      return next(new APIError(401, "Token inválido"));
+    }
+    req.user = user;
+    return next();
+  });
 }
 
 module.exports = authMiddleware;
