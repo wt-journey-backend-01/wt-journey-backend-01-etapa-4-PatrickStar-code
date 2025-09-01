eca48be4ef80ecf7340a47be7c83c453e1988ac3
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2025-09-01 13:15:09 -0300

    .

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const usuariosController = require("../controllers/authController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/register", usuariosController.cadastro);
 
@@ -8,8 +9,8 @@ router.post("/login", usuariosController.login);
 
 router.post("/logout", usuariosController.logout);
 
-router.delete("/users/:id", usuariosController.deleteUser);
+router.delete("/users/:id", authMiddleware, usuariosController.deleteUser);
 
-router.get("/usuarios/me", usuariosController.findMe);
+router.get("/usuarios/me", authMiddleware, usuariosController.findMe);
 
 module.exports = router;
