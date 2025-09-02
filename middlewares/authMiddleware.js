3862323a4dee32831e75fdacc1e3431d2615fb89
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2025-09-02 15:20:17 -0300

    .

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,54 +1,34 @@
 const jwt = require("jsonwebtoken");
 
 class APIError extends Error {
-  constructor(message, status = 500, details = {}) {
+  constructor(status, message) {
     super(message);
     this.status = status;
-    this.details = details;
     this.name = "APIError";
   }
 }
 
 function authMiddleware(req, res, next) {
-  try {
-    // Pega o token tanto do cookie quanto do header
-    const cookieToken = req.cookies?.access_token;
-    const authHeader = req.headers["authorization"];
-    const headerToken = authHeader && authHeader.split(" ")[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    const token = cookieToken || headerToken;
-
-    if (!token) {
-      return next(
-        new APIError("Token de acesso não fornecido.", 401, {
-          access_token: "Token ausente",
-        })
-      );
-    }
-
-    const secret = process.env.JWT_SECRET || "segredo";
-    if (!secret) {
-      return next(new APIError("JWT_SECRET não configurado no ambiente.", 500));
-    }
-
-    // Verificação do token
-    jwt.verify(token, secret, (err, user) => {
-      if (err) {
-        return next(
-          new APIError("Token inválido ou expirado.", 401, {
-            access_token: err.message,
-          })
-        );
-      }
-
-      req.user = user;
-      next();
-    });
-  } catch (error) {
-    return next(
-      new APIError("Erro ao autenticar usuário.", 401, error.message)
-    );
+  if (!token) {
+    return next(new APIError(401, "Token necessário"));
   }
+
+  const secret = process.env.JWT_SECRET || "segredo";
+
+  if (!secret) {
+    return next(new APIError(500, "JWT_SECRET não configurado no ambiente"));
+  }
+
+  jwt.verify(token, secret, (err, user) => {
+    if (err) {
+      return next(new APIError(401, "Token inválido"));
+    }
+    req.user = user;
+    return next();
+  });
 }
 
 module.exports = authMiddleware;
